6d22562ba7a011041e8e27b317cabda394a5d9b5
Justin Junious <justinjunious@gmail.com>
2022-11-21 09:54:42 -0500

    fixed models

--- models/grade.js
+++ models/grade.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Grade.hasMany(model.Course, {
+      Grade.hasMany(models.Course, {
         as: 'result',
         foreignKey: 'gradeId'
       })
