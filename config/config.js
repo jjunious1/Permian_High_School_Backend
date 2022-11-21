8fb6aba591604ec9c5f9b4259f74ad2d3f7d6dab
Justin Junious <justinjunious@gmail.com>
2022-11-21 11:09:33 -0500

    added necessary components to host to heroku

---
+++ config/config.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+module.exports = {
+  development: {
+    database: 'permianhigh_development',
+    dialect: 'postgres'
+  },
+  test: {
+    database: 'permianhigh_database_test',
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        rejectUnathorized: false,
+        require: true
+      }
+    }
+  }
+}
